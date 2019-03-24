1792552e43d875d74c0f30bc31cf9ceed8f2ac1f
ibrahimatay <contact@ibrahimatay.com>
2019-03-24 21:59:15 +0300

    DesignPatterns.Builder - Reviewed

--- DesignPatterns.Builder/Program.cs
+++ DesignPatterns.Builder/Program.cs
@@ -2,21 +2,74 @@
 
 namespace DesignPatterns.Builder
 {
+    public class Connection
+    {
+        
+
+        public string Database { get; set; }
+        public string Server { get; set; }
+        public string UserId { get; set; }
+        public string Password { get; set; }
+        
+        public override string ToString()
+        {
+            return $"Server={Server};Database={Database};UserId={UserId};Password={Password};";
+        }
+    }
+
+    public class ConnectionBuilder
+    {
+        public Connection Connection { get; }
+
+        public ConnectionBuilder()
+        {
+            Connection = new Connection();
+        }
+        
+        public ConnectionBuilder SetDatabase(string database)
+        {
+            Connection.Database = database;
+            return this;
+        }
+
+        public ConnectionBuilder SetServer(string server)
+        {
+            Connection.Server = server;
+            return this;
+        }
+
+        public ConnectionBuilder SetUserId(string userId)
+        {
+            Connection.UserId = userId;
+            return this;
+        }
+
+        public ConnectionBuilder SetPassword(string password)
+        {
+            Connection.Password = password;
+            return this;
+        }
+
+        public Connection Build()
+        {
+            return Connection;
+        }
+    }
+    
+    
     public class Program
     {
         public static void Main(string[] args)
         {
-            PluginBuilder builder = null;
 
-            PluginDirector pluginDirector = new PluginDirector();
-            
-            builder = new ViewPluginBuilder();
-            pluginDirector.Costruct(builder);
+            Connection connection = new ConnectionBuilder()
+                .SetServer("127.0.0.1")
+                .SetDatabase("Northwind")
+                .SetUserId("Nancy")
+                .SetPassword("123456")
+                .Build();
 
-            builder = new EnginePluginBuilder();
-            pluginDirector.Costruct(builder);
-
-            Console.ReadLine();
+            Console.WriteLine(connection.ToString());
         }
     }
 }
